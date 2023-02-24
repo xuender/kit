84a61bea249aff5b23fa0f964418681eed75bbfa
miaoyin <miaoyin@woda.com>
2023-02-24 11:12:26 +0800

    feat: color debug green

--- logs/color.go
+++ logs/color.go
@@ -13,8 +13,8 @@ var (
 	_reset            = []byte{27, 91, 48, 109}
 	_colorError color = []byte{27, 91, 51, 49, 109}
 	_colorWarn  color = []byte{27, 91, 51, 51, 109}
-	_colorDebug color = []byte{27, 91, 51, 52, 109}
-	_colorTrace color = []byte{27, 91, 51, 53, 109}
+	_colorDebug color = []byte{27, 91, 51, 50, 109}
+	_colorTrace color = []byte{27, 91, 51, 54, 109}
 )
 
 func (p color) Write(data []byte) (int, error) {
