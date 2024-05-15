a3f08aff28337a829e82036564c7c6122c9bff4b
miaoyin <miaoyin@woda.com>
2024-05-15 15:29:09 +0800

    feat: native pool

--- pools/native.go
+++ pools/native.go
@@ -1,37 +1,57 @@
 package pools
 
 import (
-	"github.com/xuender/kit/sync"
+	"sync"
+
+	ksync "github.com/xuender/kit/sync"
 )
 
 // Native 原生的协程控制.
-type Native[T any] struct {
-	yield func(T, int)
-	group *sync.RoutineGroup
+type Native[I, O any] struct {
+	yield func(I, int) O
+	group *ksync.RoutineGroup
 }
 
 // NewNative 新建原生的协程控制.
-func NewNative[T any](size uint, yield func(T, int)) *Native[T] {
-	return &Native[T]{
+func NewNative[I any, O any](size uint, yield func(I, int) O) *Native[I, O] {
+	return &Native[I, O]{
 		yield,
-		sync.NewRoutineGroup(int32(size)),
+		ksync.NewRoutineGroup(int32(size)),
 	}
 }
 
 // Post 发送数据.
-func (p *Native[T]) Post(elems ...T) {
+func (p *Native[I, O]) Post(elems ...I) []O {
+	length := len(elems)
+	if length == 0 {
+		return nil
+	}
+
+	var wait sync.WaitGroup
+
+	wait.Add(length)
+
+	ret := make([]O, length)
+
 	for idx, elem := range elems {
 		p.group.Incr()
-		go p.yield(elem, idx)
+
+		go func(num int, item I, list []O) {
+			list[num] = p.yield(item, num)
+		}(idx, elem, ret)
 	}
+
+	wait.Wait()
+
+	return ret
 }
 
 // Close 关闭协程池.
-func (p *Native[T]) Close() {
+func (p *Native[I, O]) Close() {
 	p.group.Wait()
 }
 
 // Wait 等待完毕.
-func (p *Native[T]) Wait() {
+func (p *Native[I, O]) Wait() {
 	p.group.Wait()
 }
