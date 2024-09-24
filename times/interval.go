b942bf04d3b3ca41dc1ec49af9bf66491522ed0f
xuender <xuender@gmail.com>
2024-09-24 10:06:31 +0800

    doc: times.Throttle, times.Interval

--- times/interval.go
+++ times/interval.go
@@ -2,6 +2,8 @@ package times
 
 import "time"
 
+// Interval 返回一个函数，该函数确保每次调用时都经过一段特定的时间间隔。
+// interval 参数指定了两次调用之间所需的最小时间间隔。
 func Interval(interval time.Duration) func() {
 	before := time.Now()
 
--- times/throttle.go
+++ times/throttle.go
@@ -5,16 +5,24 @@ import (
 	"time"
 )
 
+// Throttle 创建一个节流控制器，用于控制函数的调用频率。
+// 参数:
+//   - yield: 要节流的函数。
+//   - interval: 间隔时间。
+//
+// 返回值:
+//   - 第一个返回的函数用于触发节流事件。
+//   - 第二个返回的函数用于阻塞调用，直到节流函数执行完毕。
 func Throttle(yield func(), interval time.Duration) (func(), func()) {
 	var (
-		mutex   sync.Mutex
-		last    time.Time
-		timer   *time.Timer
-		group   sync.WaitGroup
-		firstMu sync.Once
+		mutex sync.Mutex
+		last  time.Time
+		timer *time.Timer
+		group sync.WaitGroup
+		first sync.Once
 	)
 
-	firstMu.Do(func() {
+	first.Do(func() {
 		last = time.Now()
 	})
 
