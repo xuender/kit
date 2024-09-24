87a71fa5465802193ec4cb444ee31e193dda27d4
xuender <xuender@gmail.com>
2024-09-24 08:50:11 +0800

    test: times.Debounce

--- times/debounce_example_test.go
+++ times/debounce_example_test.go
@@ -9,12 +9,11 @@ import (
 
 func ExampleDebounce() {
 	play, wait := times.Debounce(func() { fmt.Println("play") }, time.Millisecond)
+	defer wait()
 
 	for range 10 {
 		play()
 	}
-
-	wait()
 
 	// Output:
 	// play
